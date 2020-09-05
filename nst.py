82720e231dd353677082cf06c877c6e9f09a9afe
Narendran G <narensundaram007@gmail.com>
2020-09-05 17:07:59 +0530

    Shutdown properly

--- nst.py
+++ nst.py
@@ -66,7 +66,11 @@ class NST(object):
         process = psutil.Process(self.chrome.service.process.pid)
         for child_process in process.children(recursive=True):
             log.debug(f"Killing child process: ({child_process.pid}) - {child_process.name()} [{child_process.status()}]")
-            child_process.kill()
+            
+            try:
+                child_process.kill()
+            except psutil.NoSuchProcess as _:
+                log.debug("Already a dead process.")
         
         log.debug(f"Killing main process: ({process.pid}) - {process.name()} [{process.status()}]")
         process.kill()
